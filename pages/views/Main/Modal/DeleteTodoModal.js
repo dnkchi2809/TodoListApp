3dcc0aaa4098feebade7185dfbf38ac33cc5860d
chidnk <chidnk@devcamp.edu.vn>
2022-08-25 09:24:57 +0700

    todo-basic

--- pages/views/Main/Modal/DeleteTodoModal.js
+++ pages/views/Main/Modal/DeleteTodoModal.js
@@ -21,7 +21,7 @@ function DeleteTodoModal() {
     return (
         <>
             {/*Modal*/}
-            <div id="deleteModal" tabindex="-1" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center inset-0 bg-gray-200 bg-opacity-70 transition-opacity">
+            <div id="deleteModal" tabindex="-1" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center inset-0 bg-gray-200 bg-opacity-60 transition-opacity">
                 <div class="relative p-4 w-full max-w-md h-full md:h-auto ">
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                         <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal" onClick={onCancelClick}>
